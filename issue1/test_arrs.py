ac917e8cc46733882784ea277a03ea606ca24a10
ps'killer <komliev1999@mail.ru>
2024-02-19 01:49:04 +0500

    bug fix

--- issue1/test_arrs.py
+++ issue1/test_arrs.py
@@ -3,7 +3,6 @@ import unittest
 from issue1 import arrs
 
 
-
 class test_arrs(unittest.TestCase):
 
     def test_arrs(self)-> object:
@@ -11,5 +10,5 @@ class test_arrs(unittest.TestCase):
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
     def test_arrs(self) -> object:
-        self.assertEqual(arrs.my_slice([1, 2, 3,4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
